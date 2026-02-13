35b8b6b5b069ed8836e1c4ba866788bc5bc3da4e
fresnel25 <ngaleufresnel@gmail.com>
2026-02-13 14:14:38 +0100

    mise à jours du cors coté back

--- Backend_Project_Manager_App/index.js
+++ Backend_Project_Manager_App/index.js
@@ -10,7 +10,7 @@ const app = express();
 const cors = require("cors");
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "*",
   credentials: true
 }));
 app.use(express.json());
--- Frontend_Project_Manager_App/vite.config.js
+++ Frontend_Project_Manager_App/vite.config.js
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-})
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: true,
+    port: 5173,
+    strictPort: true,
+    allowedHosts: [process.env.VITE_ALLOWED_HOST || "localhost"],
+  },
+  preview: {
+    host: true,
+    port: 5173,
+  },
+});
